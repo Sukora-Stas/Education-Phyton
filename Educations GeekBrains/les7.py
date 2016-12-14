a011465bdc3c23a7e4d8b53f3235ed23038d1682
Sukora-Stas <stasiksukora@mail.ru>
2016-12-15 02:02:42 +0300

    lesson 8

--- Educations GeekBrains/les7.py
+++ Educations GeekBrains/les7.py
@@ -29,12 +29,19 @@ draw_circle(5, 'red')
 phi = 360 / 7
 r = 50
 
-for i in range(0, 7):
+for i in range(0, random.randrange(7, 100)):
     phi_rad = phi * i * math.pi / 180.0
     gotoxy(math.sin(phi_rad) * r, math.cos(phi_rad) * r + 60)
     turtle.circle(22)
+    draw_circle(22, 'brown')
+    draw_circle(22, 'white')
 
+gotoxy(math.sin(phi_rad) * r, math.cos(phi_rad) * r + 60)
 draw_circle(22, 'brown')
+
+if i % 7 == 0:
+    gotoxy(-150, 250)
+    turtle.write("Вы проиграли", font=("Arial", 18, "normal"))
 
 answer = ''
 while answer != 'n':
