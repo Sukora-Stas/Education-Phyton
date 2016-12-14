638e145abc4dfd5a0d70befe7eff88d4b52298ed
Sukora-Stas <stasiksukora@mail.ru>
2016-12-15 02:21:22 +0300

    lesson 9

--- Educations GeekBrains/les7.py
+++ Educations GeekBrains/les7.py
@@ -4,6 +4,9 @@ import turtle
 import random
 import math
 
+PHI = 360 / 7
+R = 50
+
 
 def gotoxy(x, y):
     turtle.penup()
@@ -20,20 +23,33 @@ def draw_circle(r, color):
 
 turtle.speed(0)
 
-gotoxy(0, 0)
-turtle.circle(80)
-gotoxy(0, 160)
 
-draw_circle(5, 'red')
+def draw_pistol(base_x, base_y):
+    gotoxy(base_x, base_y)
+    turtle.circle(80)
+    gotoxy(base_x, base_y + 160)
+    draw_circle(5, 'red')
 
-phi = 360 / 7
-r = 50
+    for i in range(0, 7):
+        phi_rad = PHI * i * math.pi / 180.0
+        gotoxy(base_x + math.sin(phi_rad) * R, base_y + math.cos(phi_rad) * R + 60)
+        draw_circle(22, 'white')
 
-for i in range(0, 7):
-    phi_rad = phi * i * math.pi / 180.0
-    gotoxy(math.sin(phi_rad) * r, math.cos(phi_rad) * r + 60)
-    draw_circle(22, 'white')
 
+def rotate_pistol(base_x, base_y, start):
+    for i in range(start, random.randrange(0, 100)):
+        phi_rad = PHI * i * math.pi / 180.0
+        gotoxy(base_x + math.sin(phi_rad) * R, base_y + math.cos(phi_rad) * R + 60)
+        turtle.circle(22)
+        draw_circle(22, 'brown')
+        draw_circle(22, 'white')
+
+    gotoxy(base_x+math.sin(phi_rad) * R, base_y+math.cos(phi_rad) * R + 60)
+    draw_circle(22, 'brown')
+    return i % 7
+
+
+draw_pistol(0, 0)
 
 answer = ''
 start = 0
@@ -41,17 +57,9 @@ while answer != 'n':
     answer = turtle.textinput("Играть?", "Y/N")
     if answer == 'y':
 
-        for i in range(start, random.randrange(0, 100)):
-            phi_rad = phi * i * math.pi / 180.0
-            gotoxy(math.sin(phi_rad) * r, math.cos(phi_rad) * r + 60)
-            turtle.circle(22)
-            draw_circle(22, 'brown')
-            draw_circle(22, 'white')
+        start = rotate_pistol(100, 100, start)
 
-        gotoxy(math.sin(phi_rad) * r, math.cos(phi_rad) * r + 60)
-        draw_circle(22, 'brown')
-        start = i % 7
-        if i % 7 == 0:
+        if start == 0:
             gotoxy(-150, 250)
             turtle.write("Вы проиграли", font=("Arial", 18, "normal"))
 
@@ -62,5 +70,5 @@ while answer != 'n':
             # turtle.begin_fill()
             # turtle.circle(random.randrange(1, 100))
             # turtle.end_fill()
-    else:
-        pass
+else:
+    pass
